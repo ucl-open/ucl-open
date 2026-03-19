83bcce17fcefd9b066359ba6c794ef106c7c2517
NeuroThom <t.ryan@neurogears.org>
2026-03-19 12:29:09 +0000

    Add screen to default vision imports

--- src/ucl_open/vision/__init__.py
+++ src/ucl_open/vision/__init__.py
@@ -1,4 +1,4 @@
 from ucl_open.vision.vision import SyncQuad, RandomFlip
-from ucl_open.vision.displays import DisplayIntrinsics, DisplayExtrinsics, DisplayCalibration
+from ucl_open.vision.displays import DisplayIntrinsics, DisplayExtrinsics, DisplayCalibration, Screen
 
-__all__ = ["SyncQuad", "RandomFlip", "DisplayIntrinsics", "DisplayExtrinsics", "DisplayCalibration"]
+__all__ = ["SyncQuad", "RandomFlip", "DisplayIntrinsics", "DisplayExtrinsics", "DisplayCalibration", "Screen"]
