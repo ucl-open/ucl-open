4167f440a63d68b2e6ed98df76dd60502fc63a7d
RoboDoig <erskine94@gmail.com>
2026-05-24 15:48:46 +0100

    Refactor display schemas to better reflect workflow patterns

--- src/ucl_open/vision/displays.py
+++ src/ucl_open/vision/displays.py
@@ -5,9 +5,15 @@ import ucl_open.core.base as Types
 from swc.aeon.schema import BaseSchema
 
 
+class ViewportConfiguration(BaseSchema):
+    width: float = Field(default=1, ge=0, le=0, description="The width of the viewport as a fraction of total screen width")
+    height: float = Field(default=1, ge=0, le=0, description="The height of the viewport as a fraction of total screen width")
+    x: float = Field(default=0, ge=0, le=0, description="The x-coordinate of the lower left corner of the viewport as a fraction of total screen width")
+    y: float = Field(default=0, ge=0, le=0, description="The y-coordinate of the lower left corner of the viewport as a fraction of total screen height")
+
+
 class DisplayIntrinsics(BaseSchema):
-    frame_width: int = Field(default=1920, ge=0, description="Pixel frame width")
-    frame_height: int = Field(default=1080, ge=0, description="Pixel frame height")
+    viewport_configuration: ViewportConfiguration = Field(default = ViewportConfiguration(), description="The viewport configuration for this display intrinsic")
     display_width: float = Field(default=20, ge=0, description="Physical display width")
     display_height: float = Field(default=15, ge=0, description="Physical display height")
 
@@ -32,6 +38,8 @@ class DisplayCalibration(BaseSchema):
 
 class Screen(BaseSchema):
     display_index: int = Field(default=1, description="Display index")
+    window_width: int = Field(default=1920, ge=0, description="Width of the screen in pixels")
+    window_height: int = Field(default=1080, ge=0, description="Height of the screen in pixels")
     target_render_frequency: float = Field(default=60, description="Target render frequency")
     target_update_frequency: float = Field(default=120, description="Target update frequency")
     texture_assets_directory: str = Field(default="Textures", description="Calibration directory")
