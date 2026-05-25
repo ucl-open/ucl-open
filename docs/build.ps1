97e498f0dbdd17e0432b82862fa392485673761b
NeuroThom <t.ryan@neurogears.org>
2026-05-26 00:18:22 +0100

    Fix Bonsai bootstrapper download in build.ps1

--- docs/build.ps1
+++ docs/build.ps1
@@ -7,13 +7,21 @@ $PSNativeCommandUseErrorActionPreference = $true
 
 Push-Location $PSScriptRoot
 try {
-    $bonsaiExe = Join-Path $PSScriptRoot '.bonsai\Bonsai.exe'
+    $bonsaiDir = Join-Path $PSScriptRoot '.bonsai'
+    $bonsaiExe = Join-Path $bonsaiDir 'Bonsai.exe'
     if (-not (Test-Path $bonsaiExe)) {
         Write-Host "Downloading Bonsai bootstrapper..."
-        $bonsaiConfig = [xml](Get-Content (Join-Path $PSScriptRoot '.bonsai\Bonsai.config'))
+        $bonsaiConfig = [xml](Get-Content (Join-Path $bonsaiDir 'Bonsai.config'))
         $bonsaiVersion = ($bonsaiConfig.PackageConfiguration.Packages.Package | Where-Object { $_.id -eq 'Bonsai' }).version
-        $downloadUrl = "https://github.com/bonsai-rx/bonsai/releases/download/$bonsaiVersion/Bonsai.exe"
-        Invoke-WebRequest -Uri $downloadUrl -OutFile $bonsaiExe
+        $downloadUrl = "https://github.com/bonsai-rx/bonsai/releases/download/$bonsaiVersion/Bonsai.zip"
+        $tempZip = Join-Path $bonsaiDir 'temp.zip'
+        $nugetConfig = Join-Path $bonsaiDir 'NuGet.config'
+        $tempNuget = Join-Path $bonsaiDir 'temp.config'
+        Invoke-WebRequest -Uri $downloadUrl -OutFile $tempZip
+        Move-Item -Path $nugetConfig -Destination $tempNuget -ErrorAction SilentlyContinue
+        Expand-Archive -Path $tempZip -DestinationPath $bonsaiDir -Force
+        Move-Item -Path $tempNuget -Destination $nugetConfig -Force -ErrorAction SilentlyContinue
+        Remove-Item -Path $tempZip
     }
     Write-Host "Bootstrapping Bonsai environment..."
     & $bonsaiExe --no-editor
