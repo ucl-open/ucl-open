e3e1570061bb86e29a365f4629a957d45a43b5cd
NeuroThom <t.ryan@neurogears.org>
2026-05-25 23:39:25 +0100

    Bootstrap bonsai env in build.ps1

--- docs/build.ps1
+++ docs/build.ps1
@@ -7,6 +7,17 @@ $PSNativeCommandUseErrorActionPreference = $true
 
 Push-Location $PSScriptRoot
 try {
+    $bonsaiExe = Join-Path $PSScriptRoot '.bonsai\Bonsai.exe'
+    if (-not (Test-Path $bonsaiExe)) {
+        Write-Host "Downloading Bonsai bootstrapper..."
+        $bonsaiConfig = [xml](Get-Content (Join-Path $PSScriptRoot '.bonsai\Bonsai.config'))
+        $bonsaiVersion = ($bonsaiConfig.PackageConfiguration.Packages.Package | Where-Object { $_.id -eq 'Bonsai' }).version
+        $downloadUrl = "https://github.com/bonsai-rx/bonsai/releases/download/$bonsaiVersion/Bonsai.exe"
+        Invoke-WebRequest -Uri $downloadUrl -OutFile $bonsaiExe
+    }
+    Write-Host "Bootstrapping Bonsai environment..."
+    & $bonsaiExe --no-editor
+
     $libPaths = @()
     $libPaths += Get-ChildItem "..\artifacts\bin\*\release_net4*" -Directory | Select-Object -Expand FullName
     $libPaths += "..\artifacts\package\release"
